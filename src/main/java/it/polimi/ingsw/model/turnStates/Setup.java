2090583d0f3f0f36cf7574c79a19560b5d316fb6
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-03-23 18:59:27 +0100

    feat: add TurnState interface and one turnState

---
+++ src/main/java/it/polimi/ingsw/model/turnStates/Setup.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.model.turnStates;
+
+public class Setup implements TurnState {
+}
---
+++ src/main/java/it/polimi/ingsw/model/turnStates/TurnState.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.model.turnStates;
+
+public interface TurnState {
+}
