f4d1709c96f805db8a1db53adb3f30568e225002
Andrea Riva <riva.andrea98@gmail.com>
2020-03-25 22:56:30 +0100

    chore(model/gods): start implementing TurnEvents for Apollo

--- src/main/java/it/polimi/ingsw/model/gods/Apollo.java
+++ src/main/java/it/polimi/ingsw/model/gods/Apollo.java
@@ -2,7 +2,12 @@ package it.polimi.ingsw.model.gods;
 
 import it.polimi.ingsw.model.Turn;
 import it.polimi.ingsw.model.Worker;
+import it.polimi.ingsw.model.actions.MoveAction;
+import it.polimi.ingsw.model.board.Cell;
+import it.polimi.ingsw.model.board.TargetCells;
 import it.polimi.ingsw.model.turnevents.TurnEvents;
+
+import java.util.List;
 
 /**
  * The god card Apollo.
@@ -14,37 +19,33 @@ class Apollo extends AbstractGod {
     private static final TurnEvents ownerTurnEvents = new TurnEvents(){
         @Override
         protected void onBeforeMovement(Turn turn) {
-            //TODO
-            /*
-            Cell currentCell = worker.getCell();
-            TargetCells walkableCells = worker.getWalkableCells();
-            TargetCells surroundingCells = TargetCells.fromCellAndRadius(worker.getCell(), 1);
-            for (Cell targetCell : surroundingCells.getTargets()) {
-                targetCell.getWorker().ifPresent(targetWorker -> {
-                    if (targetCell.isWalkable() && Cell.computeHeightDifference(targetCell, currentCell) <= 1 && !targetWorker.getPlayer().equals(turn.getPlayer())) {
-                        walkableCells.addTargets(targetCell);
-                    }
-                });
+            Worker[] workers = turn.getPlayer().getOwnWorkers();
+            for (Worker worker : workers) {
+                Cell currentCell = worker.getCell();
+                //TODO
+//                TargetCells walkableCells = turn.getWalkableCells(worker);
+//                TargetCells surroundingCells = TargetCells.fromCellAndRadius(worker.getCell(), 1);
+//                for (Cell targetCell : surroundingCells.getTargets()) {
+//                    targetCell.getWorker().ifPresent(targetWorker -> {
+//                        if (targetCell.isWalkable() && Cell.computeHeightDifference(targetCell, currentCell) <= 1 && !targetWorker.getPlayer().equals(turn.getPlayer())) {
+//                            walkableCells.addTargets(targetCell);
+//                        }
+//                    });
+//                }
             }
-             */
         }
 
         @Override
         protected void onAfterMovement(Turn turn) {
-            //TODO
-            /*
-            try {
-                MoveAction lastAction = (MoveAction) turn.getAction(-1);
-                lastAction.targetCell.getWorker().ifPresent(targetWorker -> {
-                    if (!targetWorker.getPlayer().equals(turn.getPlayer())) {
-                        game.setWorkerCell(targetWorker, lastAction.sourceCell);
-                    }
-                });
-            }
-            catch (ClassCastException e) {
-                //TODO
-            }
-             */
+            List<MoveAction> moveActions = turn.getMoves();
+            //TODO: shall we check there is at least 1 move? After all, if we got here, there at least 1 move was performed
+            MoveAction lastMove = moveActions.get(moveActions.size() - 1);
+            lastMove.getTargetCell().getWorker().ifPresent(targetWorker -> {
+                if (!targetWorker.getPlayer().equals(turn.getPlayer())) {
+//                    TODO
+//                    game.setWorkerCell(targetWorker, lastAction.sourceCell);
+                }
+            });
         }
     };
 
