c3455386210a10f43726806ea82afac6afff82bf
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-03-25 21:30:21 +0100

    feat: add Action interface

---
+++ src/main/java/it/polimi/ingsw/model/actions/Action.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.model.actions;
+
+/**
+ * This interface provides a common access point to all the actions which can be performed
+ * during a turn
+ */
+public interface Action {
+}
