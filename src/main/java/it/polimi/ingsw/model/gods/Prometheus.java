92016da759817486bfdf61fc440a1a362e3af8e2
Andrea Riva <riva.andrea98@gmail.com>
2020-03-25 23:44:05 +0100

    feat(model/gods): start implementing TurnEvents for Prometheus

--- src/main/java/it/polimi/ingsw/model/gods/Prometheus.java
+++ src/main/java/it/polimi/ingsw/model/gods/Prometheus.java
@@ -1,7 +1,15 @@
 package it.polimi.ingsw.model.gods;
 
 import it.polimi.ingsw.model.Turn;
+import it.polimi.ingsw.model.Worker;
+import it.polimi.ingsw.model.actions.BuildAction;
+import it.polimi.ingsw.model.actions.MoveAction;
+import it.polimi.ingsw.model.board.Board;
+import it.polimi.ingsw.model.board.Cell;
+import it.polimi.ingsw.model.board.TargetCells;
 import it.polimi.ingsw.model.turnevents.TurnEvents;
+
+import java.util.List;
 
 /**
  * The god card Prometheus.
@@ -23,29 +31,35 @@ class Prometheus extends AbstractGod {
         @Override
         protected void onBeforeMovement(Turn turn) {
             //TODO
-            /*
-           turn.setAllowSkipBuild(false);
-            List<BuildAction> lastBuildActions = turn.getBuildActions();
+//            turn.setAllowSkipBuild(false);
+            List<BuildAction> lastBuildActions = turn.getBuilds();
             if (lastBuildActions.size() > 0) {
-                BuildAction lastBuild = (BuildAction) lastBuildActions.get(0);
-                turn.clearAllowedWorkers();
-                turn.addAllowedWorker(lastBuild.worker);
-                worker.getWalkableCells.removeTargets(lastMove.sourceCell);
-                TargetCells higherCells = TargetCells.fromMinimumHeight(worker.getCell().getHeight());
-                worker.getWalkableCells().removeTargets(higherCells);
+                BuildAction lastBuild = lastBuildActions.get(0);
+                Worker lastBuildWorker = lastBuild.getPerformer();
+//                TODO
+//                turn.clearAllowedWorkers();
+//                turn.addAllowedWorker(lastBuild.worker);
+                TargetCells notHigherCells = new TargetCells();
+                notHigherCells.setAllTargets(false);
+//                TODO
+//                turn.getGame().getBoard().getCellsList().stream()
+//                        .filter(
+//                                cell -> cell.getTower().getCurrentLevel() <= lastBuildWorker.getCell().getTower().getCurrentLevel()
+//                        )
+//                        .forEach(
+//                                cell -> notHigherCells.setPosition(cell, true)
+//                        );
+                turn.getWorkerWalkableCells(lastBuildWorker).intersect(notHigherCells);
             }
-             */
         }
 
         @Override
         protected void onAfterBuild(Turn turn) {
-            //TODO
-            /*
-            List<MoveAction> lastMoveActions = turn.getMoveActions();
+            List<MoveAction> lastMoveActions = turn.getMoves();
             if (lastMoveActions.size() == 0) {
-                turn.setNextState(TurnState.BEFORE_MOVE);
+//                TODO
+//                turn.setNextState(TurnState.BEFORE_MOVE);
             }
-             */
         }
     };
 
