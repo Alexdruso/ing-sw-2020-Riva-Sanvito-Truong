eaa945889b1b29a7a06a97038640b6b9d20e0341
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-03-27 21:16:18 +0100

    feat: add setup method to TurnState

--- src/main/java/it/polimi/ingsw/model/turnstates/TurnState.java
+++ src/main/java/it/polimi/ingsw/model/turnstates/TurnState.java
@@ -7,6 +7,14 @@ import it.polimi.ingsw.model.workers.Worker;
 public abstract class TurnState {
 
     /**
+     * This method sets things up before we can use the other methods provided by the state
+     * @param turn the Context
+     */
+    public void setup(Turn turn){
+
+    }
+
+    /**
      * This method sets up the first actual state of the turn and performs
      * some default calculation on the buildableCells and walkableCells
      * @param turn the Context
