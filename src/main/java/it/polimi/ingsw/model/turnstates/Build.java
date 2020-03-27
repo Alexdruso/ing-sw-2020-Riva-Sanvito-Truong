21d906b23a9df1ba40bfc05174b117113d432d39
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-03-27 10:30:47 +0100

    feat: turnstates

--- src/main/java/it/polimi/ingsw/model/turnstates/Build.java
+++ src/main/java/it/polimi/ingsw/model/turnstates/Build.java
@@ -1,4 +1,124 @@
 package it.polimi.ingsw.model.turnstates;
 
+import it.polimi.ingsw.model.Turn;
+import it.polimi.ingsw.model.board.Cell;
+import it.polimi.ingsw.model.workers.Worker;
+
 public class Build implements TurnState {
+    /**
+     * This method sets up the first actual state of the turn and performs
+     * some default calculation on the buildableCells and walkableCells
+     *
+     * @param turn the Context
+     */
+    @Override
+    public void startTurn(Turn turn) {
+
+    }
+
+    /**
+     * This boolean methods checks if the pawn can move to targetCell
+     *
+     * @param pawn       the worker we want to move
+     * @param targetCell the cell we want to move the worker to
+     * @param turn       the Context
+     * @return if the pawn can move to targetCell
+     */
+    @Override
+    public boolean canMoveTo(Worker pawn, Cell targetCell, Turn turn) {
+        return false;
+    }
+
+    /**
+     * This method moves the pawn to targetCell
+     *
+     * @param pawn       the worker we want to move
+     * @param targetCell the cell we want to move the worker to
+     * @param turn       the Context
+     */
+    @Override
+    public void moveTo(Worker pawn, Cell targetCell, Turn turn) {
+
+    }
+
+    /**
+     * This boolean methods checks if the pawn can build a Dome in targetCell
+     *
+     * @param pawn       the worker who performs the build
+     * @param targetCell the cell involved in the build
+     * @param turn       the Context
+     * @return true if the pawn can build dome in targetCell
+     */
+    @Override
+    public boolean canBuildDomeIn(Worker pawn, Cell targetCell, Turn turn) {
+        return false;
+    }
+
+    /**
+     * This methods builds a dome in targetCell
+     *
+     * @param pawn       the worker who performs the build
+     * @param targetCell the cell involved in the build
+     * @param turn       the Context
+     */
+    @Override
+    public void buildDomeIn(Worker pawn, Cell targetCell, Turn turn) {
+
+    }
+
+    /**
+     * This boolean methods checks if the pawn can build a block in targetCell
+     *
+     * @param pawn       the worker who performs the build
+     * @param targetCell the cell involved in the build
+     * @param turn       the Context
+     * @return true if the pawn can build a block in targetCell
+     */
+    @Override
+    public boolean canBuildBlockIn(Worker pawn, Cell targetCell, Turn turn) {
+        return false;
+    }
+
+    /**
+     * This methods builds a block in targetCell
+     *
+     * @param pawn       the worker who performs the build
+     * @param targetCell the cell involved in the build
+     * @param turn       the Context
+     */
+    @Override
+    public void buildBlockIn(Worker pawn, Cell targetCell, Turn turn) {
+
+    }
+
+    /**
+     * This method lets the player surrender
+     *
+     * @param turn the Context
+     */
+    @Override
+    public void draw(Turn turn) {
+
+    }
+
+    /**
+     * This method checks if we can end the turn
+     *
+     * @param turn the Context
+     * @return if the player can end the turn
+     */
+    @Override
+    public boolean canEndTurn(Turn turn) {
+        return false;
+    }
+
+    /**
+     * This method ends the turn
+     *
+     * @param turn the Context
+     */
+    @Override
+    public void endTurn(Turn turn) {
+
+    }
 }
