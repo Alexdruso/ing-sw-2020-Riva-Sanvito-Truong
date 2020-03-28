9aa1415955596763956079aea56125a6743b7994
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-03-28 20:49:31 +0100

    feat: add methods to be implemented

--- src/main/java/it/polimi/ingsw/model/turnstates/Win.java
+++ src/main/java/it/polimi/ingsw/model/turnstates/Win.java
@@ -1,4 +1,37 @@
 package it.polimi.ingsw.model.turnstates;
 
-class Win {
+import it.polimi.ingsw.model.Turn;
+
+class Win extends AbstractTurnState{
+    /**
+     * This method sets things up before we can use the other methods provided by the state
+     *
+     * @param turn the Context
+     */
+    @Override
+    public void setup(Turn turn) {
+
+    }
+
+    /**
+     * This method checks if we can end the turn
+     *
+     * @param turn the Context
+     * @return if the player can end the turn
+     */
+    @Override
+    public boolean canEndTurn(Turn turn) {
+        return super.canEndTurn(turn);
+    }
+
+    /**
+     * This method ends the turn
+     *
+     * @param turn the Context
+     * @throws InvalidTurnStateException if not in the right state
+     */
+    @Override
+    public void endTurn(Turn turn) throws InvalidTurnStateException {
+        super.endTurn(turn);
+    }
 }
