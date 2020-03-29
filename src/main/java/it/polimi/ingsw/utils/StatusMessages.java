3bc878cf12cebfa30ddcb29b5ac112ebc3d04da2
JohnSmith114 <kientuong@gmail.com>
2020-03-29 23:27:27 +0200

    feat: add statusMessages to handle communications from Controller to View

---
+++ src/main/java/it/polimi/ingsw/utils/StatusMessages.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.utils;
+
+public enum StatusMessages {
+    OK,
+    MOVE_ERROR,
+    BUILD_ERROR,
+    SKIP_ERROR,
+    VICTORY,
+    LOSS,
+    YOUR_TURN,
+    OPPONENT_TURN
+}
