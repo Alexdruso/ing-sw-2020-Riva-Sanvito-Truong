505b5bff58d489f014e0d4b15593686440d05d67
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-03-29 12:48:21 +0200

    feat: add getPerformer as a Action interface method

--- src/main/java/it/polimi/ingsw/model/actions/Action.java
+++ src/main/java/it/polimi/ingsw/model/actions/Action.java
@@ -1,8 +1,14 @@
 package it.polimi.ingsw.model.actions;
 
+import it.polimi.ingsw.model.workers.Worker;
+
 /**
  * This interface provides a common access point to all the actions which can be performed
  * during a turn
  */
 public interface Action {
+    /**
+     * @return The worker who performed the action
+     */
+    public Worker getPerformer();
 }
