43f8b95577b4de70c360f8107fe2ff8fd2c01bc4
JohnSmith114 <kientuong@gmail.com>
2020-03-30 00:38:18 +0200

    feat: update player commands

--- src/main/java/it/polimi/ingsw/model/playercommands/PlayerBuildCommand.java
+++ src/main/java/it/polimi/ingsw/model/playercommands/PlayerBuildCommand.java
@@ -1,6 +1,71 @@
 package it.polimi.ingsw.model.playercommands;
 
+import it.polimi.ingsw.model.board.Buildable;
+import it.polimi.ingsw.model.board.Cell;
+import it.polimi.ingsw.model.workers.Worker;
+
 public class PlayerBuildCommand extends PlayerCommand {
+    /**
+     * The cell on which the worker intends to build
+     */
+    private Cell targetCell;
+
+    /**
+     * The component that is to be built on the cell
+     */
+    private Buildable component;
+
+    /**
+     * The level that is to be built by the worker.
+     */
+    private int builtLevel;
+
+    /**
+     * The worker who is to perform the build
+     */
+    private Worker performer;
+
+    /**
+     * Constructor, stores all the variables by reference
+     * @param targetCell The cell on which the worker built
+     * @param component The component built on the cell
+     * @param builtLevel The level built by the worker
+     * @param performer The worker who performed the build
+     */
+    public PlayerBuildCommand(Cell targetCell, Buildable component, int builtLevel, Worker performer) {
+        this.targetCell = targetCell;
+        this.component = component;
+        this.builtLevel = builtLevel;
+        this.performer = performer;
+    }
+
+    /**
+     * @return The cell on which the worker built
+     */
+    public Cell getTargetCell() {
+        return targetCell;
+    }
+
+    /**
+     * @return The component built on the cell
+     */
+    public Buildable getComponent() {
+        return component;
+    }
+
+    /**
+     * @return The level built by the worker
+     */
+    public int getBuiltLevel() {
+        return builtLevel;
+    }
+
+    /**
+     * @return The worker who performed the build
+     */
+    public Worker getPerformer() {
+        return performer;
+    }
 
     /**
      * This method returns the type of the current action
--- src/main/java/it/polimi/ingsw/model/playercommands/PlayerMoveCommand.java
+++ src/main/java/it/polimi/ingsw/model/playercommands/PlayerMoveCommand.java
@@ -1,6 +1,59 @@
 package it.polimi.ingsw.model.playercommands;
 
+import it.polimi.ingsw.model.board.Cell;
+import it.polimi.ingsw.model.workers.Worker;
+
+/**
+ * This class represents a command given by the player to move a worker to another cell.
+ */
 public class PlayerMoveCommand extends PlayerCommand {
+    /**
+     * The cell from which the worker moved
+     */
+    private Cell sourceCell;
+
+    /**
+     * The cell to which the worker moved
+     */
+    private Cell targetCell;
+
+    /**
+     * The worker who performed the move
+     */
+    private Worker performer;
+
+    /**
+     * Constructor, stores all the variables by reference
+     * @param sourceCell The cell from which the worker moved
+     * @param targetCell The cell to which the worker moved
+     * @param performer The worker who performed the move
+     */
+    public PlayerMoveCommand(Cell sourceCell, Cell targetCell, Worker performer) {
+        this.sourceCell = sourceCell;
+        this.targetCell = targetCell;
+        this.performer = performer;
+    }
+
+    /**
+     * @return The cell from which the worker moved
+     */
+    public Cell getSourceCell() {
+        return sourceCell;
+    }
+
+    /**
+     * @return The cell to which the worker moved
+     */
+    public Cell getTargetCell() {
+        return targetCell;
+    }
+
+    /**
+     * @return The worker who performed the move
+     */
+    public Worker getPerformer() {
+        return performer;
+    }
 
     /**
      * This method returns the type of the current action
