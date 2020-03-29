9fbf45ffa367fe16b16e775b99f4a44b5baa9334
JohnSmith114 <kientuong@gmail.com>
2020-03-29 21:46:30 +0200

    feat: add view stub

---
+++ src/main/java/it/polimi/ingsw/view/View.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.view;
+
+import it.polimi.ingsw.model.playercommands.PlayerCommand;
+import it.polimi.ingsw.observer.Observable;
+import it.polimi.ingsw.observer.Observer;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class View extends Observable<PlayerCommand> implements Observer<Object> {
+    @Override
+    public void update(Object message) {
+        //TODO: define the type for Observer
+        throw new NotImplementedException();
+    }
+}
--- src/main/java/it/polimi/ingsw/view/keep.java
+++
@@ -1,4 +0,0 @@
-package it.polimi.ingsw.view;
-
-public class keep {
-}
