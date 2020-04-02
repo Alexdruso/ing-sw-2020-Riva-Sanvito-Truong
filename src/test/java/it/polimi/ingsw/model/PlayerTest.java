2280a3c4b359c6ef889df574153991e202182416
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-04-02 20:25:30 +0200

    test: add PlayerTest

---
+++ src/test/java/it/polimi/ingsw/model/PlayerTest.java
@@ -0,0 +1,90 @@
+package it.polimi.ingsw.model;
+
+import it.polimi.ingsw.model.gods.God;
+import it.polimi.ingsw.model.turnevents.TurnEventsManager;
+import it.polimi.ingsw.model.workers.WorkerID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class PlayerTest {
+    God godMock;
+
+    @BeforeEach
+    void godSetup(){
+        godMock = mock(God.class);
+        when(godMock.getName()).thenReturn("Thor");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "Pippo", "Pluto", "Kek", "" })
+    void setAndGetNickname(String name) {
+        Player testPlayer = new Player(name);
+        assertEquals(testPlayer.getNickname(),name);
+        testPlayer.setNickname(name);
+        assertEquals(testPlayer.getNickname(),name);
+    }
+
+    @Test
+    void getOwnWorkers() {
+        Player testPlayer = new Player("Kek");
+
+        for(int i=0; i < testPlayer.getOwnWorkers().length; i++){
+            assertEquals(testPlayer.getOwnWorkers()[i].getWorkerID(), WorkerID.values()[i]);
+            assertEquals(testPlayer.getOwnWorkers()[i].getPlayer(), testPlayer);
+        }
+    }
+
+    @Test
+    void setAndGetGod() {
+        Player testPlayer = new Player("Kek");
+        testPlayer.setGod(godMock);
+        assertEquals(godMock, testPlayer.getGod());
+    }
+
+    @Test
+    void getTurnEventsManager() {
+        Player testPlayer = new Player("Kek");
+        testPlayer.setGod(godMock);
+
+        assertEquals(testPlayer.getTurnEventsManager().getClass(), TurnEventsManager.class);
+    }
+
+    @Test
+    void testEquals() {
+        Player[] players = new Player[]{new Player("Pippo"),
+                new Player("Pippo"),
+                new Player("Pluto"),
+                new Player(""),
+                new Player(null)
+        };
+
+        assertEquals(players[0],players[0]);
+        assertEquals(players[0], players[1]);
+        assertNotEquals(players[0],players[2]);
+        assertNotEquals(players[0],players[3]);
+        assertNotEquals(players[0],players[4]);
+    }
+
+    @Test
+    void testHashCode() {
+        Player[] players = new Player[]{new Player("Pippo"),
+                new Player("Pippo"),
+                new Player("Pluto"),
+                new Player(""),
+                new Player(null)
+        };
+
+        assertEquals(players[0].hashCode(),players[0].hashCode());
+        assertEquals(players[0].hashCode(), players[1].hashCode());
+        assertNotEquals(players[0].hashCode(),players[2].hashCode());
+        assertNotEquals(players[0].hashCode(),players[3].hashCode());
+        assertNotEquals(players[0].hashCode(),players[4].hashCode());
+    }
+}
