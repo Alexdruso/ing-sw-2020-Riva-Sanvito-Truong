24428a3885778760db64b91e951ef33ccf7ef29a
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-04-03 19:14:25 +0200

    test: add LoseTest

---
+++ src/test/java/it/polimi/ingsw/model/turnstates/LoseTest.java
@@ -0,0 +1,35 @@
+package it.polimi.ingsw.model.turnstates;
+
+import it.polimi.ingsw.model.Turn;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+class LoseTest {
+    Turn mockTurn = mock(Turn.class);
+    Lose loseTest = new Lose();
+
+    @Test
+    void setup() {
+        loseTest.setup(this.mockTurn);
+        verify(this.mockTurn).setNextState(TurnState.LOSE.getTurnState());
+    }
+
+    @Test
+    void canEndTurn() {
+        assertTrue(loseTest.canEndTurn(this.mockTurn));
+    }
+
+    @Test
+    void endTurn() {
+        try{
+            this.loseTest.endTurn(this.mockTurn);
+            assertTrue(true);
+        }
+        catch (Exception e){
+            fail();
+        }
+    }
+}
