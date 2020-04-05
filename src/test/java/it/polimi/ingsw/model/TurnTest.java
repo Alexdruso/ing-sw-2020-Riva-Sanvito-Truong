2b9143ddf2fba038ff777022d1c34d5b2e38c7ab
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-04-05 19:02:31 +0200

    test: add test of standard turn flow

--- src/test/java/it/polimi/ingsw/model/TurnTest.java
+++ src/test/java/it/polimi/ingsw/model/TurnTest.java
@@ -6,14 +6,10 @@ import it.polimi.ingsw.model.board.Board;
 import it.polimi.ingsw.model.board.Cell;
 import it.polimi.ingsw.model.board.Component;
 import it.polimi.ingsw.model.board.TargetCells;
-import it.polimi.ingsw.model.turnstates.AbstractTurnState;
+import it.polimi.ingsw.model.turnevents.TurnEventsManager;
+import it.polimi.ingsw.model.turnstates.InvalidTurnStateException;
 import it.polimi.ingsw.model.turnstates.TurnState;
-import it.polimi.ingsw.model.workers.Worker;
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -91,60 +87,117 @@ class TurnTest {
 
     @Test
     void testLosingTurn(){
+        //Our own default TurnEventsManager
+        TurnEventsManager myTurnEventsManager = mock(TurnEventsManager.class);
+        //Funny easter egg for JoJo fans
+        Player myPlayer = spy(new Player("Giorno Giovanna"));
+        //The trick to make it work without a god
+        when(myPlayer.getTurnEventsManager()).thenReturn(myTurnEventsManager);
+        //Mock game 'cause it's too complex
+        Game myGame = spy(new Game());
+        //A real board because why not
+        Board myBoard = spy(new Board());
+        //Give Game a meaning
+        when(myGame.getBoard()).thenReturn(myBoard);
+        //Create the turn
+        Turn myTurn = spy(new Turn(myGame, myPlayer));
+    }
+
+    @Test void testWinningTurn(){
+        //Our own default TurnEventsManager
+        TurnEventsManager myTurnEventsManager = mock(TurnEventsManager.class);
+        //Funny easter egg for JoJo fans
+        Player myPlayer = spy(new Player("Giorno Giovanna"));
+        //The trick to make it work without a god
+        when(myPlayer.getTurnEventsManager()).thenReturn(myTurnEventsManager);
+        //Mock game 'cause it's too complex
+        Game myGame = spy(new Game());
+        //A real board because why not
+        Board myBoard = spy(new Board());
+        //Give Game a meaning
+        when(myGame.getBoard()).thenReturn(myBoard);
+        //Create the turn
+        Turn myTurn = spy(new Turn(myGame, myPlayer));
+
 
     }
 
     @Test
-    void testWinningTurn(){
+    void testStandardTurn() throws InvalidTurnStateException {
+        //Our own default TurnEventsManager
+        TurnEventsManager myTurnEventsManager = mock(TurnEventsManager.class);
+        //Funny easter egg for JoJo fans
+        Player myPlayer = spy(new Player("Giorno Giovanna"));
+        //The trick to make it work without a god
+        when(myPlayer.getTurnEventsManager()).thenReturn(myTurnEventsManager);
+        //Mock game 'cause it's too complex
+        Game myGame = spy(new Game());
+        //A real board because why not
+        Board myBoard = spy(new Board());
+        //Give Game a meaning
+        when(myGame.getBoard()).thenReturn(myBoard);
 
+        //Setup the state
+        myPlayer.getOwnWorkers()[0].setCell(myBoard.getCell(1,1));
+        myBoard.getCell(1,1).setWorker(myPlayer.getOwnWorkers()[0]);
+        myPlayer.getOwnWorkers()[1].setCell(myBoard.getCell(3,3));
+        myBoard.getCell(3,3).setWorker(myPlayer.getOwnWorkers()[1]);
+        //Create the turn
+        Turn myTurn = spy(new Turn(myGame, myPlayer));
+        //Start the turn
+        myTurn.startTurn();
+        //Just another check that all is right
+        assertFalse(myTurn.canMoveTo(myPlayer.getOwnWorkers()[1],myBoard.getCell(0,0)));
+        //Now we check if we can move :)
+        assertTrue(myTurn.canMoveTo(myPlayer.getOwnWorkers()[0],myBoard.getCell(0,0)));
+        //Now move
+        myTurn.moveTo(myPlayer.getOwnWorkers()[0],myBoard.getCell(0,0));
+        //Check if movement happened
+        assertEquals(myPlayer.getOwnWorkers()[0].getCell(),myBoard.getCell(0,0));
+        assertEquals(myPlayer.getOwnWorkers()[0],myBoard.getCell(0,0).getWorker().get());
+        assertEquals(1, myTurn.getMoves().size());
+        //Check if movement was registered
+        assertEquals(myTurn.getPerformedAction().get(0).getPerformer(),myPlayer.getOwnWorkers()[0]);
+        //Check that worker 2 is not in allowedWorkers anymore
+        assertFalse(myTurn.getAllowedWorkers().contains(myPlayer.getOwnWorkers()[1]));
+        //Trigger exception
+        try{myTurn.moveTo(myPlayer.getOwnWorkers()[0],myBoard.getCell(0,0));}
+        catch (Exception e){ assertTrue(true);}
+        //Now we check and build a block where we were
+        assertTrue(myTurn.canBuildBlockIn(myPlayer.getOwnWorkers()[0],myBoard.getCell(1,1)));
+        myTurn.buildBlockIn(myPlayer.getOwnWorkers()[0],myBoard.getCell(1,1));
+        //Check the build happened
+        assertEquals(1, myBoard.getCell(1, 1).getTower().getCurrentLevel());
+        assertEquals(myTurn.getPerformedAction().get(1).getPerformer(),myPlayer.getOwnWorkers()[0]);
+        assertEquals(1, myTurn.getBuilds().size());
+        //Check if we can end turn
+        assertTrue(myTurn.canEndTurn());
+        //End turn :)
+        myTurn.endTurn();
     }
 
     @Test
-    void testStandardTurn(){
+    void testTurnWithDraw(){
+        //Our own default TurnEventsManager
+        TurnEventsManager myTurnEventsManager = mock(TurnEventsManager.class);
+        //Funny easter egg for JoJo fans
+        Player myPlayer = spy(new Player("Giorno Giovanna"));
+        //The trick to make it work without a god
+        when(myPlayer.getTurnEventsManager()).thenReturn(myTurnEventsManager);
+        //Mock game 'cause it's too complex
+        Game mockGame = mock(Game.class);
+        //A real board because why not
+        Board myBoard = spy(new Board());
+        //Give Game a meaning
+        when(mockGame.getBoard()).thenReturn(myBoard);
+        //Create the turn
+        Turn myTurn = spy(new Turn(mockGame, myPlayer));
 
-    }
-
-    @Test
-    void triggerLosingTurn() {
-    }
-
-    @Test
-    void changeState() {
-    }
-
-    @Test
-    void startTurn() {
-    }
-
-    @Test
-    void canMoveTo() {
-    }
-
-    @Test
-    void moveTo() {
-    }
-
-    @Test
-    void canBuildDomeIn() {
-    }
-
-    @Test
-    void buildDomeIn() {
-    }
-
-    @Test
-    void canBuildBlockIn() {
-    }
-
-    @Test
-    void buildBlockIn() {
-    }
-
-    @Test
-    void draw() {
-    }
-
-    @Test
-    void endTurn() {
+        //Oh no, we decide to draw immediately, just like France
+        myTurn.draw();
+        verify(myTurn).triggerLosingTurn();
+        verify(myTurn,times(2)).setNextState(TurnState.LOSE.getTurnState());
+        verify(myTurn).changeState();
+        assertTrue(myTurn.canEndTurn());
     }
 }
