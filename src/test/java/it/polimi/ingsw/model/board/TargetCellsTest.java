955da303e1bbfda5353843d4310b6977de409571
JohnSmith114 <kientuong@gmail.com>
2020-04-06 16:42:46 +0200

    refactor: remove accidental import

--- src/test/java/it/polimi/ingsw/model/board/TargetCellsTest.java
+++ src/test/java/it/polimi/ingsw/model/board/TargetCellsTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import javax.swing.*;
-import java.lang.annotation.Target;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
