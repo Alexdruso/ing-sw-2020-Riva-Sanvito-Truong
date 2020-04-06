3258702af3b3a490ecd84452a05855926f98e6c2
JohnSmith114 <kientuong@gmail.com>
2020-04-06 19:28:55 +0200

    docs: add JavaDocs to Direction class

--- src/main/java/it/polimi/ingsw/model/board/Direction.java
+++ src/main/java/it/polimi/ingsw/model/board/Direction.java
@@ -1,8 +1,24 @@
 package it.polimi.ingsw.model.board;
 
+/**
+ * This immutable class represents a direction, which is the vector representing the displacement from a Cell to another
+ */
 public class Direction {
+    /**
+     * The displacement on the x axis
+     */
     public final int dx;
+
+    /**
+     * The displacement on the y axis
+     */
     public final int dy;
+
+    /**
+     * The class constructor.
+     * @param firstCell
+     * @param secondCell
+     */
     public Direction(Cell firstCell, Cell secondCell){
         dx = secondCell.getX() - firstCell.getX();
         dy = secondCell.getY() - firstCell.getY();
