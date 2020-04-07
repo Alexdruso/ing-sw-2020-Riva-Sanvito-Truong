2e0f44027449d397c3178ad79acfa37561628753
Andrea Riva <7723068+a-riva@users.noreply.github.com>
2020-04-07 21:46:47 +0200

    test(model/actions): add tests for BuildAction

---
+++ src/test/java/it/polimi/ingsw/model/actions/BuildActionTest.java
@@ -0,0 +1,40 @@
+package it.polimi.ingsw.model.actions;
+
+import it.polimi.ingsw.model.board.Buildable;
+import it.polimi.ingsw.model.board.Cell;
+import it.polimi.ingsw.model.gods.GodCard;
+import it.polimi.ingsw.model.gods.GodsTestHarness;
+import it.polimi.ingsw.model.workers.Worker;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class BuildActionTest {
+    private final Cell cell = mock(Cell.class);
+    private final Buildable buildable = mock(Buildable.class);
+    private final int builtLevel = 123;
+    private final Worker worker = mock(Worker.class);
+    private final BuildAction buildAction = new BuildAction(cell, buildable, builtLevel, worker);
+
+    @Test
+    void getTargetCell() {
+        assertEquals(buildAction.getTargetCell(), cell);
+    }
+
+    @Test
+    void getComponent() {
+        assertEquals(buildAction.getComponent(), buildable);
+    }
+
+    @Test
+    void getBuiltLevel() {
+        assertEquals(buildAction.getBuiltLevel(), builtLevel);
+    }
+
+    @Test
+    void getPerformer() {
+        assertEquals(buildAction.getPerformer(), worker);
+    }
+}
