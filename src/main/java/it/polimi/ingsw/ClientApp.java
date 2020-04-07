71a7d2a62a235008df1151ff6f1e56a8f3b40eed
Andrea Riva <7723068+a-riva@users.noreply.github.com>
2020-04-07 15:33:24 +0200

    chore: add placeholders for client and server

---
+++ src/main/java/it/polimi/ingsw/ClientApp.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw;
+
+public class ClientApp {
+    public static void main(String[] args) {
+        //TODO
+
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/ServerApp.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw;
+
+public class ServerApp {
+    public static void main(String[] args) {
+        //TODO
+    }
+}
