d9c0509d737eef72434c9848f0bf0822ebe89154
Andrea Riva <7723068+a-riva@users.noreply.github.com>
2020-04-13 00:43:17 +0200

    feat: implement test ServerApp [skip ci]

--- src/main/java/it/polimi/ingsw/ServerApp.java
+++ src/main/java/it/polimi/ingsw/ServerApp.java
@@ -1,7 +1,50 @@
 package it.polimi.ingsw;
 
+import it.polimi.ingsw.observer.Observer;
+import it.polimi.ingsw.utils.StatusMessages;
+import it.polimi.ingsw.utils.messages.ClientSetNicknameMessage;
+import it.polimi.ingsw.utils.networking.Connection;
+import it.polimi.ingsw.utils.networking.Transmittable;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerApp {
     public static void main(String[] args) {
-        //TODO
+        //TODO: write real implementation. At the moment, this is just a boilerplate
+        try {
+            ServerSocket ss = new ServerSocket(1337);
+            while (true) {
+                try {
+                    Socket s = ss.accept();
+                    System.out.println("accepted connection");
+                    Connection connection = new Connection(s);
+                    connection.addObserver(new ServerTestReceiver(connection));
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class ServerTestReceiver implements Observer<Transmittable> {
+    Connection connection;
+
+    public ServerTestReceiver(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public void update(Transmittable message) {
+        System.out.println("received message");
+        if (message instanceof ClientSetNicknameMessage) {
+            System.out.println("was set nickname: " + ((ClientSetNicknameMessage) message).getNickname());
+        }
+        connection.send(StatusMessages.OK);
     }
 }
