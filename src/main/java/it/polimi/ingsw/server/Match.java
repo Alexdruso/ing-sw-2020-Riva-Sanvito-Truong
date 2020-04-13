af1b8adaec1e63ce89b94842a2eaab9f7858d3e3
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-04-13 22:28:43 +0200

    feat: add Match

--- src/main/java/it/polimi/ingsw/server/Match.java
+++ src/main/java/it/polimi/ingsw/server/Match.java
@@ -3,60 +3,21 @@ package it.polimi.ingsw.server;
 import it.polimi.ingsw.controller.Controller;
 import it.polimi.ingsw.controller.User;
 import it.polimi.ingsw.model.Game;
-import it.polimi.ingsw.model.gods.God;
-import it.polimi.ingsw.observer.Observer;
+import it.polimi.ingsw.utils.messages.ServerStartSetupMatchMessage;
 import it.polimi.ingsw.utils.networking.Connection;
-import it.polimi.ingsw.utils.networking.Transmittable;
 import it.polimi.ingsw.view.View;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
 
-public class Match implements Runnable, Observer<Transmittable> {
-    /**
-     * The structure which holds the connections related to the game
-     */
-    private final Connection[] connections;
-    /**
-     * The structures which holds all the nicknames of the joining players
-     */
-    private final String[] nicknames;
-
-    /**
-     * Constructor of a 2 players match
-     * @param user1 the first player
-     * @param nickname1 the first player's nickname
-     * @param user2 the second player
-     * @param nickname2 the second player's nickname
-     */
-    public Match(Connection user1, String nickname1, Connection user2, String nickname2) {
-        //initialize connections and nicknames data structure
-        this.connections = new Connection[]{user1, user2};
-        this.nicknames = new String[]{nickname1,nickname2};
-        for(Connection connection:connections){
-            connection.addObserver(this);
-        }
-    }
-
-    /**
-     * Constructor of a 3 players match
-     * @param user1 the first player
-     * @param nickname1 the first player's nickname
-     * @param user2 the second player
-     * @param nickname2 the second player's nickname
-     * @param user3 the third player
-     * @param nickname3 the third player's nickname
-     */
-    public Match(Connection user1, String nickname1, Connection user2, String nickname2, Connection user3, String nickname3) {
-        //initialize connections and nicknames data structure
-        this.connections = new Connection[]{user1, user2, user3};
-        this.nicknames = new String[]{nickname1,nickname2,nickname3};
-        for(Connection connection:connections){
-            connection.addObserver(this);
-        }
-    }
+/**
+ * This class is designed to be able to run on a separate thread and initialize all the classes
+ * needed to start the game, in particular Game,VirtualView and Controller.
+ * Before calling the "run" method, the server should add nicknames and connections in order.
+ */
+public class Match implements Runnable{
+    LinkedHashMap<Connection, String> participants = new LinkedHashMap<Connection,String>();
 
     /**
      * When an object implementing interface <code>Runnable</code> is used
@@ -67,40 +28,68 @@ public class Match implements Runnable, Observer<Transmittable> {
      * The general contract of the method <code>run</code> is that it may
      * take any action whatsoever.
      *
+     *
      * @see Thread#run()
      */
     @Override
     public void run() {
-        //TODO send match started notification
-        //TODO ask god sublist to connections[0] (this shall be a broadcast message with nickname as payload)
-        //TODO wait for an answer
-        //TODO send confirmation
-        //the structures which is to hold the God chosen by the player
-        Map<String, God> chosenGods = new HashMap<String, God>();
-        //TODO ask to chose own god to each player
-        //instantiate the game
-        Game model = new Game(this.nicknames.length);
-        //the structure to hold the views
-        List<View> views = new LinkedList<View>();
-        //setup game and views
-        for (String nickname : nicknames) {
-            User user = new User(nickname);
-            model.subscribeUser(nickname, chosenGods.get(nickname));
-            //TODO initialize a virtualView w/ right parameters
-            views.add(new View());
-        }
-        //TODO change controller initialization with right parameters
+        //create a new game
+        Game model = new Game(this.participants.size());
+        //create the controller
         Controller controller = new Controller(model);
-        //TODO start game setup
+        //Create the views and add the player to the Game
+        LinkedList<View> virtualViews = new LinkedList<View>();
+        for(Connection connection : this.participants.keySet()){
+            //create the view
+            View virtualView = new View(connection,this.participants.get(connection));
+            //get the user from the view
+            User user = virtualView.getUser();
+            //add the user as a player in the model
+            model.subscribeUser(user);
+            //the view observes the model
+            model.addObserver(virtualView);
+            //the controller observes the view
+            //TODO remove comment when controller implements Observer<Transmittable>
+            //virtualView.addObserver(controller);
+            //add virtualView to virtualViews list
+            virtualViews.add(virtualView);
+        }
+        //Start the game setup
+        ServerStartSetupMatchMessage serverStartSetupMatchMessage = new ServerStartSetupMatchMessage(
+                (User[]) this.participants.values().stream().map(User::new).toArray()
+        );
+        for(Connection connection : this.participants.keySet()){
+            connection.send(serverStartSetupMatchMessage);
+        }
     }
 
     /**
-     * This method is called by the Observable to update the observer
-     *
-     * @param message the message to be received
+     * Adds the connection and the nickname to the match own participants
+     * @param connection the connection of the participant
+     * @param nickname the nickname chosen by the participants
      */
-    @Override
-    public void update(Transmittable message) {
-
+    public void addParticipant(Connection connection, String nickname){
+        this.participants.put(connection,nickname);
     }
+
+    /**
+     * Adds all the participants from the parameter to the match own participants.
+     * It should be called only before executing run.
+     * @param participants the structure holding the participants to be added
+     */
+    public void addParticipants(LinkedHashMap<Connection,String> participants){
+        this.participants.putAll(participants);
+    }
+
+    /**
+     * Returns the participants to the match, represented by their connection and nickname
+     * @return the participants to the match
+     */
+    public LinkedHashMap<Connection,String> getParticipants(){
+        return new LinkedHashMap<Connection,String>(this.participants);
+    }
+
+    //TODO delet dis
+    public Match(Connection connection1, String nickname1, Connection connection2, String nickname2){}
+    public Match(Connection connection1, String nickname1, Connection connection2, String nickname2, Connection connection3, String nickname3){}
 }
