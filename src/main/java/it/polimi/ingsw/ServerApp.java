65fa1581613809c2579c4ee97fb8d3c9c2177818
JohnSmith114 <kientuong@gmail.com>
2020-04-13 02:17:46 +0200

    feat: change ServerApp stub to the official implementation

--- src/main/java/it/polimi/ingsw/ServerApp.java
+++ src/main/java/it/polimi/ingsw/ServerApp.java
@@ -1,50 +1,18 @@
 package it.polimi.ingsw;
 
-import it.polimi.ingsw.observer.Observer;
-import it.polimi.ingsw.utils.StatusMessages;
-import it.polimi.ingsw.utils.messages.ClientSetNicknameMessage;
-import it.polimi.ingsw.utils.networking.Connection;
-import it.polimi.ingsw.utils.networking.Transmittable;
+import it.polimi.ingsw.server.Server;
 
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 public class ServerApp {
     public static void main(String[] args) {
-        //TODO: write real implementation. At the moment, this is just a boilerplate
-        try {
-            ServerSocket ss = new ServerSocket(1337);
-            while (true) {
-                try {
-                    Socket s = ss.accept();
-                    System.out.println("accepted connection");
-                    Connection connection = new Connection(s);
-                    connection.addObserver(new ServerTestReceiver(connection));
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        } catch (IOException e) {
+        Server server;
+        try{
+            server = new Server();
+            server.start();
+        } catch (IOException e){
             e.printStackTrace();
+            System.out.println("Could not initialize server");
         }
-    }
-}
-
-class ServerTestReceiver implements Observer<Transmittable> {
-    Connection connection;
-
-    public ServerTestReceiver(Connection connection) {
-        this.connection = connection;
-    }
-
-    @Override
-    public void update(Transmittable message) {
-        System.out.println("received message");
-        if (message instanceof ClientSetNicknameMessage) {
-            System.out.println("was set nickname: " + ((ClientSetNicknameMessage) message).getNickname());
-        }
-        connection.send(StatusMessages.OK);
     }
 }
