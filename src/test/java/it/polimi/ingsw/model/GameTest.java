aee92ec133bce3a0b7c780f85d0c4ac9ab9893e3
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-04-13 22:04:17 +0200

    refactor: change GameTest to fit the new subscribeUser signature

--- src/test/java/it/polimi/ingsw/model/GameTest.java
+++ src/test/java/it/polimi/ingsw/model/GameTest.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.controller.User;
 import it.polimi.ingsw.model.gods.GodCard;
 import org.junit.jupiter.api.Test;
 
@@ -12,24 +13,24 @@ public class GameTest {
     @Test
     public void cannotRegisterMoreUsersThanDeclared(){
         Game game = new Game(3);
-        game.subscribeUser("Romano Prodi", GodCard.ATHENA.getGod());
-        game.subscribeUser("Sghiribizou", GodCard.APOLLO.getGod());
-        game.subscribeUser("Nick Name", GodCard.ARTEMIS.getGod());
+        game.subscribeUser(new User("Romano Prodi"), GodCard.ATHENA.getGod());
+        game.subscribeUser(new User("Sghiribizou"), GodCard.APOLLO.getGod());
+        game.subscribeUser(new User("Nick Name"), GodCard.ARTEMIS.getGod());
         assertThrows(IllegalStateException.class,
-                ()-> game.subscribeUser("Luccio", GodCard.DEMETER.getGod()));
+                ()-> game.subscribeUser(new User("Luccio"), GodCard.DEMETER.getGod()));
         Game game2 = new Game(2);
-        game2.subscribeUser("Nagito Komaeda", GodCard.ARTEMIS.getGod());
-        game2.subscribeUser("Francesco Totti", GodCard.APOLLO.getGod());
+        game2.subscribeUser(new User("Nagito Komaeda"), GodCard.ARTEMIS.getGod());
+        game2.subscribeUser(new User("Francesco Totti"), GodCard.APOLLO.getGod());
         assertThrows(IllegalStateException.class,
-                () -> game.subscribeUser("Genoveffo Brombeis", GodCard.HEPHAESTUS.getGod()));
+                () -> game.subscribeUser(new User("Genoveffo Brombeis"), GodCard.HEPHAESTUS.getGod()));
     }
 
     @Test
     public void testRegisterPlayers(){
         Game game = new Game(3);
-        game.subscribeUser("Maria Ortigli", GodCard.APOLLO.getGod());
-        game.subscribeUser("Buggo", GodCard.ARTEMIS.getGod());
-        game.subscribeUser("Zeb89", GodCard.DEMETER.getGod());
+        game.subscribeUser(new User("Maria Ortigli"), GodCard.APOLLO.getGod());
+        game.subscribeUser(new User("Buggo"), GodCard.ARTEMIS.getGod());
+        game.subscribeUser(new User("Zeb89"), GodCard.DEMETER.getGod());
         List<Player> playerList = game.getPlayersList();
         assertEquals(playerList.get(0).getNickname(), "Maria Ortigli");
         assertSame(playerList.get(0).getGod(), GodCard.APOLLO.getGod());
