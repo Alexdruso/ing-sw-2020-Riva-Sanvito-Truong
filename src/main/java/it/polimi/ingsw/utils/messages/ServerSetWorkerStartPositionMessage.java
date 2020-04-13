7da0199c018aa88408ad6eead1984a15d4eed14d
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-04-13 16:49:49 +0200

    feat: add ServerSetWorkerStartPositionMessage

---
+++ src/main/java/it/polimi/ingsw/utils/messages/ServerSetWorkerStartPositionMessage.java
@@ -0,0 +1,42 @@
+package it.polimi.ingsw.utils.messages;
+
+import it.polimi.ingsw.model.workers.WorkerID;
+
+public class ServerSetWorkerStartPositionMessage extends ServerMessage {
+    /**
+     * The x coordinate of the cell to which the worker is positioned
+     */
+    public final int targetCellX;
+
+    /**
+     * The y coordinate of the cell to which the worker is positioned
+     */
+    public final int targetCellY;
+
+    /**
+     * The positioned worker
+     */
+    public final WorkerID performer;
+
+    /**
+     * Constructor, stores all the variables by reference
+     * @param targetCellX The x coordinate of the cell to which the worker is positioned
+     * @param targetCellY The y coordinate of the cell to which the worker is positioned
+     * @param performer The positioned worker
+     */
+    public ServerSetWorkerStartPositionMessage(int targetCellX, int targetCellY, WorkerID performer) {
+        super();
+        this.targetCellX = targetCellX;
+        this.targetCellY = targetCellY;
+        this.performer = performer;
+    }
+    /**
+     * This method returns the type of the current action
+     *
+     * @return the type of the current action, as an instance of PlayerActions
+     */
+    @Override
+    public ServerMessages getMessageType() {
+        return ServerMessages.SET_WORKER_START_POSITION;
+    }
+}
