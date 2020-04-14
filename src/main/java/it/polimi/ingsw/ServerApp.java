8232214754fa0d37608086dec6e7a3b2c1335c52
Andrea Riva <riva.andrea98@gmail.com>
2020-04-14 23:57:16 +0200

    chore: restore to actual implementation of ServerApp

--- src/main/java/it/polimi/ingsw/ServerApp.java
+++ src/main/java/it/polimi/ingsw/ServerApp.java
@@ -1,11 +1,8 @@
-//TODO: DO NOT MERGE ME!!
-
 package it.polimi.ingsw;
 
-import it.polimi.ingsw.controller.User;
 import it.polimi.ingsw.observer.Observer;
 import it.polimi.ingsw.utils.StatusMessages;
-import it.polimi.ingsw.utils.messages.*;
+import it.polimi.ingsw.utils.messages.ClientSetNicknameMessage;
 import it.polimi.ingsw.utils.networking.Connection;
 import it.polimi.ingsw.utils.networking.Transmittable;
 
@@ -42,47 +39,11 @@ class ServerTestReceiver implements Observer<Transmittable> {
         this.connection = connection;
     }
 
-    void waitAndSendMatchStart() {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        User[] users = new User[]{new User("nick1"), new User("nick2"), new User("nick3")};
-        connection.send(new ServerStartMatchMessage(users));
-    }
-
     @Override
     public void update(Transmittable message) {
         System.out.println("received message");
-        if (message instanceof ClientDisconnectMessage) {
-            System.out.println("client disconnected");
-            connection.close();
-            return;
-        }
         if (message instanceof ClientSetNicknameMessage) {
-            String nick = ((ClientSetNicknameMessage) message).getNickname();
-            System.out.println("was set nickname: " + nick);
-            if (nick.equals("nak")) {
-                connection.send(StatusMessages.CLIENT_ERROR);
-                return;
-            }
-        }
-        if (message instanceof ClientJoinLobbyMessage) {
-            if (false && Math.random() > 0.5) {
-                connection.send(StatusMessages.CONTINUE);
-                return;
-            }
-            else {
-                connection.send(StatusMessages.OK);
-                waitAndSendMatchStart();
-                return;
-            }
-        }
-        if (message instanceof ClientSetPlayersCountMessage) {
-            connection.send(StatusMessages.OK);
-            waitAndSendMatchStart();
-            return;
+            System.out.println("was set nickname: " + ((ClientSetNicknameMessage) message).getNickname());
         }
         connection.send(StatusMessages.OK);
     }
