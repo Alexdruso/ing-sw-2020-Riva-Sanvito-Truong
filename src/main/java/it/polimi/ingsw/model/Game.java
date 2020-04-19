06aaf3f36121be66876369731a959cd026a6c9f2
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-04-19 18:24:50 +0200

    feat: add board initialization in Game

--- src/main/java/it/polimi/ingsw/model/Game.java
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -58,6 +58,7 @@ public class Game extends Observable<Transmittable> {
         playersInGame = MAX_NUMBER_OF_PLAYERS;
         subscribedUsers = new LinkedHashMap<>();
         lastRound = new LinkedList<>();
+        board = new Board();
     }
 
     /**
