e8409643638c70f4b564aaeb4fb1062244f29300
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-04-25 17:00:31 +0200

    feat: add ServerMoveMessage

---
+++ src/main/java/it/polimi/ingsw/utils/messages/ServerMoveMessage.java
@@ -0,0 +1,54 @@
+package it.polimi.ingsw.utils.messages;
+
+import it.polimi.ingsw.client.Client;
+import it.polimi.ingsw.controller.User;
+import it.polimi.ingsw.model.workers.WorkerID;
+import it.polimi.ingsw.utils.networking.ClientHandleable;
+
+public class ServerMoveMessage extends ServerMessage implements ClientHandleable {
+
+
+    /**
+     * The x coordinate of the cell to which the worker moved
+     */
+    public final int targetCellX;
+
+    /**
+     * The y coordinate of the cell to which the worker moved
+     */
+    public final int targetCellY;
+
+    /**
+     * The worker who performed the move
+     */
+    public final WorkerID performer;
+
+    /**
+     * Constructor, stores all the variables by reference
+     *
+     * @param targetCellX The x coordinate of the cell to which the worker moved
+     * @param targetCellY The y coordinate of the cell to which the worker moved
+     * @param performer   The worker who performed the move
+     */
+    public ServerMoveMessage(User user, int targetCellX, int targetCellY, WorkerID performer) {
+        super(user);
+        this.targetCellX = targetCellX;
+        this.targetCellY = targetCellY;
+        this.performer = performer;
+    }
+
+    /**
+     * This method returns the type of the current action
+     *
+     * @return the type of the current action, as an instance of PlayerActions
+     */
+    @Override
+    public ServerMessages getMessageType() {
+        return ServerMessages.MOVE;
+    }
+
+    @Override
+    public boolean handleTransmittable(Client handler) {
+        return false;
+    }
+}
