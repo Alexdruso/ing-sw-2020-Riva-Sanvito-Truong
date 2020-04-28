0434780331778b5f73ce214e3e80a06aec2be7de
Kien Tuong Truong <kientuong@gmail.com>
2020-04-29 01:35:02 +0200

    feat: start working on gui [skip ci]

--- src/main/java/it/polimi/ingsw/JavaFXApp.java
+++ src/main/java/it/polimi/ingsw/JavaFXApp.java
@@ -1,19 +1,39 @@
 package it.polimi.ingsw;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+
 public class JavaFXApp extends Application {
+
+    private static Stage primaryStage;
+
     @Override
     public void start(Stage stage) throws Exception {
+        primaryStage = stage;
+
         stage.setTitle("Santorini - GC02");
-        Label label = new Label("testlabel");
-        Scene scene = new Scene(label, 1920, 1080);
+
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainMenu.fxml"));
+        Scene scene = new Scene(root, 1920, 1080);
+
+        scene.getStylesheets().add(getClass().getResource("/css/MainMenu.css").toExternalForm());
+        scene.getStylesheets().add(getClass().getResource("/css/common.css").toExternalForm());
+
         stage.setScene(scene);
         stage.setFullScreen(true);
         stage.show();
+    }
+
+    public static Stage getPrimaryStage(){
+        return primaryStage;
+    }
+
+    public static void setPrimaryStage(Stage stage){
+        primaryStage = stage;
     }
 
     public static void main(String[] args) {
