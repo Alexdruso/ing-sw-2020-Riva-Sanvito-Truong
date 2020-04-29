1f4d3cca5ffe8b6624967f585834689c70559ee5
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-04-29 22:16:35 +0200

    refactor: delete ServerSetCurrentUserMessage

---
+++ src/main/java/it/polimi/ingsw/utils/messages/ServerAskGodFromListMessage.java
@@ -0,0 +1,28 @@
+package it.polimi.ingsw.utils.messages;
+
+import it.polimi.ingsw.client.Client;
+import it.polimi.ingsw.controller.User;
+import it.polimi.ingsw.model.gods.GodCard;
+import it.polimi.ingsw.utils.networking.ClientHandleable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServerAskGodFromListMessage implements ServerMessage, ClientHandleable {
+    private User user;
+    private List<GodCard> godList;
+
+    public ServerAskGodFromListMessage(User user, List<GodCard> godList) {
+        this.user = user;
+        this.godList = godList;
+    }
+
+    public List<GodCard> getGodList() {
+        return new ArrayList<>(godList);
+    }
+
+    @Override
+    public boolean handleTransmittable(Client handler) {
+        return false;
+    }
+}
