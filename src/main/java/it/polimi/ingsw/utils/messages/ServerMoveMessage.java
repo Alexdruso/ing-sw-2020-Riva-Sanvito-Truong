fb9bc0f381ba7f2cd8f65feca41be32b241ba36d
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-04-29 17:48:46 +0200

    feat: add source cell coordinates to ServerMoveMessage

--- src/main/java/it/polimi/ingsw/utils/messages/ServerMoveMessage.java
+++ src/main/java/it/polimi/ingsw/utils/messages/ServerMoveMessage.java
@@ -1,8 +1,6 @@
 package it.polimi.ingsw.utils.messages;
 
 import it.polimi.ingsw.client.Client;
-
-import it.polimi.ingsw.client.reducedmodel.ReducedCell;
 import it.polimi.ingsw.controller.User;
 import it.polimi.ingsw.model.workers.WorkerID;
 import it.polimi.ingsw.utils.networking.ClientHandleable;
@@ -30,11 +28,16 @@ public class ServerMoveMessage implements ServerMessage, ClientHandleable {
 
     /**
      * Constructor, stores all the variables by reference
+     *
+     * @param user        The user
+     * @param sourceCellX The x coordinate of the cell from which the worker moved
+     * @param sourceCellY The y coordinate of the cell from which the worker moved
      * @param targetCellX The x coordinate of the cell to which the worker moved
      * @param targetCellY The y coordinate of the cell to which the worker moved
-     * @param performer The worker who performed the move
+     * @param performer   The worker who performed the move
      */
-    public ServerMoveMessage(User user, int targetCellX, int targetCellY, WorkerID performer) {
+    public ServerMoveMessage(User user, int sourceCellX, int sourceCellY,
+                             int targetCellX, int targetCellY, WorkerID performer) {
         this.user = user;
         this.targetCellX = targetCellX;
         this.targetCellY = targetCellY;
