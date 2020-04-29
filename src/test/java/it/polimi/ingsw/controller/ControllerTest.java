58a571c99eb38768f34542847ded03289684040d
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-04-29 14:48:40 +0200

    refactor: change isValidMove signature

--- src/test/java/it/polimi/ingsw/controller/ControllerTest.java
+++ src/test/java/it/polimi/ingsw/controller/ControllerTest.java
@@ -3,7 +3,9 @@ package it.polimi.ingsw.controller;
 import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.board.Component;
 import it.polimi.ingsw.utils.StatusMessages;
-import it.polimi.ingsw.utils.messages.*;
+import it.polimi.ingsw.utils.messages.ClientBuildMessage;
+import it.polimi.ingsw.utils.messages.ClientMoveMessage;
+import it.polimi.ingsw.utils.messages.ClientSkipMessage;
 import it.polimi.ingsw.view.View;
 import it.polimi.ingsw.view.ViewClientMessage;
 import org.junit.jupiter.api.Test;
@@ -20,15 +22,20 @@ class ControllerTest {
         View myView = mock(View.class);
         User myUser = mock(User.class);
         //mock the actions
-        ClientBuildMessage myBuildCommand = spy(new ClientBuildMessage(0,0, Component.BLOCK,0,null));
-        ClientMoveMessage myMoveCommand = spy(new ClientMoveMessage(0,0,0,0,null));
+        ClientBuildMessage myBuildCommand = spy(new ClientBuildMessage(0, 0, Component.BLOCK, 0, null));
+        ClientMoveMessage myMoveCommand = spy(new ClientMoveMessage(0, 0, 0, 0, null));
         ClientSkipMessage mySkipCommand = spy(new ClientSkipMessage());
         ViewClientMessage buildViewClientMessage = new ViewClientMessage(myBuildCommand, myView, myUser);
         ViewClientMessage moveViewClientMessage = new ViewClientMessage(myMoveCommand, myView, myUser);
         ViewClientMessage skipViewClientMessage = new ViewClientMessage(mySkipCommand, myView, myUser);
         //establish mock behavior, in this case a positive behavior
         when(myGame.isValidBuild(myBuildCommand, myUser)).thenReturn(true);
-        when(myGame.isValidMove(myMoveCommand, myUser)).thenReturn(true);
+        when(myGame
+                .isValidMove(
+                        myMoveCommand.sourceCellX, myMoveCommand.sourceCellY,
+                        myMoveCommand.targetCellX, myMoveCommand.targetCellY,
+                        myMoveCommand.performer, myUser))
+                .thenReturn(true);
         when(myGame.isValidSkip(mySkipCommand, myUser)).thenReturn(true);
         //ready steady go
         //create a new controller
@@ -44,7 +51,10 @@ class ControllerTest {
         //verify the right calls
         verify(myGame, times(1)).isValidBuild(myBuildCommand, myUser);
         verify(myGame, times(1)).build(myBuildCommand, myUser);
-        verify(myGame, times(1)).isValidMove(myMoveCommand, myUser);
+        verify(myGame, times(1)).isValidMove(
+                myMoveCommand.sourceCellX, myMoveCommand.sourceCellY,
+                myMoveCommand.targetCellX, myMoveCommand.targetCellY,
+                myMoveCommand.performer, myUser);
         verify(myGame, times(1)).move(myMoveCommand, myUser);
         verify(myGame, times(1)).isValidSkip(mySkipCommand, myUser);
         verify(myGame, times(1)).skip(mySkipCommand, myUser);
@@ -59,15 +69,19 @@ class ControllerTest {
         View myView = mock(View.class);
         User myUser = mock(User.class);
         //mock the actions
-        ClientBuildMessage myBuildCommand = spy(new ClientBuildMessage(0,0, Component.BLOCK,0,null));
-        ClientMoveMessage myMoveCommand = spy(new ClientMoveMessage(0,0,0,0,null));
+        ClientBuildMessage myBuildCommand = spy(new ClientBuildMessage(0, 0, Component.BLOCK, 0, null));
+        ClientMoveMessage myMoveCommand = spy(new ClientMoveMessage(0, 0, 0, 0, null));
         ClientSkipMessage mySkipCommand = spy(new ClientSkipMessage());
         ViewClientMessage buildViewClientMessage = new ViewClientMessage(myBuildCommand, myView, myUser);
         ViewClientMessage moveViewClientMessage = new ViewClientMessage(myMoveCommand, myView, myUser);
         ViewClientMessage skipViewClientMessage = new ViewClientMessage(mySkipCommand, myView, myUser);
         //establish mock behavior, in this case a negative behavior
         when(myGame.isValidBuild(myBuildCommand, myUser)).thenReturn(false);
-        when(myGame.isValidMove(myMoveCommand, myUser)).thenReturn(false);
+        when(myGame.isValidMove(
+                myMoveCommand.sourceCellX, myMoveCommand.sourceCellY,
+                myMoveCommand.targetCellX, myMoveCommand.targetCellY,
+                myMoveCommand.performer, myUser))
+                .thenReturn(false);
         when(myGame.isValidSkip(mySkipCommand, myUser)).thenReturn(false);
         //ready, steady, go
         //ready steady go
@@ -84,7 +98,10 @@ class ControllerTest {
         //verify right calls
         verify(myGame, times(1)).isValidBuild(myBuildCommand, myUser);
         verify(myGame, times(0)).build(myBuildCommand, myUser);
-        verify(myGame, times(1)).isValidMove(myMoveCommand, myUser);
+        verify(myGame, times(1)).isValidMove(
+                myMoveCommand.sourceCellX, myMoveCommand.sourceCellY,
+                myMoveCommand.targetCellX, myMoveCommand.targetCellY,
+                myMoveCommand.performer, myUser);
         verify(myGame, times(0)).move(myMoveCommand, myUser);
         verify(myGame, times(1)).isValidSkip(mySkipCommand, myUser);
         verify(myGame, times(0)).skip(mySkipCommand, myUser);
