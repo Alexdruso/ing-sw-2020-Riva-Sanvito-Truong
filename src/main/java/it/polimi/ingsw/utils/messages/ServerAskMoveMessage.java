abe3539dd1cf626b2debff4fe6a24e6c614f8770
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-05-01 22:34:47 +0200

    feat: add reducedUser to ServerAskMoveMessage

--- src/main/java/it/polimi/ingsw/utils/messages/ServerAskMoveMessage.java
+++ src/main/java/it/polimi/ingsw/utils/messages/ServerAskMoveMessage.java
@@ -8,12 +8,14 @@ import java.util.HashMap;
 import java.util.List;
 
 public class ServerAskMoveMessage implements ServerMessage, ClientHandleable {
+    public final ReducedUser user;
     public final boolean isSkippable;
     public final List<WorkerID> allowedWorkers;
     public final HashMap<WorkerID, ReducedTargetCells> workerBlockBuildableCells;
 
-    public ServerAskMoveMessage(boolean isSkippable, List<WorkerID> allowedWorkers,
+    public ServerAskMoveMessage(ReducedUser user, boolean isSkippable, List<WorkerID> allowedWorkers,
                                 HashMap<WorkerID, ReducedTargetCells> workerBlockBuildableCells) {
+        this.user = user;
         this.isSkippable = isSkippable;
         this.allowedWorkers = allowedWorkers;
         this.workerBlockBuildableCells = workerBlockBuildableCells;
