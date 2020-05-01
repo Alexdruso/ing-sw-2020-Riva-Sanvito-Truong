25b8191eb0e9019e99dbae435562724dcfae5d65
Andrea Riva <riva.andrea98@gmail.com>
2020-05-01 21:08:17 +0200

    feat(utils/messages): add equals to ReducedUser and ReducedGod

--- src/main/java/it/polimi/ingsw/utils/messages/ReducedGod.java
+++ src/main/java/it/polimi/ingsw/utils/messages/ReducedGod.java
@@ -1,11 +1,25 @@
 package it.polimi.ingsw.utils.messages;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class ReducedGod implements Serializable {
     public final String name;
 
     public ReducedGod(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ReducedGod that = (ReducedGod) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
--- src/main/java/it/polimi/ingsw/utils/messages/ReducedUser.java
+++ src/main/java/it/polimi/ingsw/utils/messages/ReducedUser.java
@@ -1,11 +1,25 @@
 package it.polimi.ingsw.utils.messages;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class ReducedUser implements Serializable {
     public final String nickname;
 
     public ReducedUser(String nickname) {
         this.nickname = nickname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ReducedUser that = (ReducedUser) o;
+        return nickname.equals(that.nickname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nickname);
     }
 }
