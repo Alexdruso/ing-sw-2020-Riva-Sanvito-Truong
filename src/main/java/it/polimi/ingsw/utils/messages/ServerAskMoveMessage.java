a96fba3694b2832fd5e59af9839b47563afa072c
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-05-01 22:25:13 +0200

    refactor: use ReducedTargetCells in ask move and build

--- src/main/java/it/polimi/ingsw/utils/messages/ServerAskMoveMessage.java
+++ src/main/java/it/polimi/ingsw/utils/messages/ServerAskMoveMessage.java
@@ -1,7 +1,6 @@
 package it.polimi.ingsw.utils.messages;
 
 import it.polimi.ingsw.client.Client;
-import it.polimi.ingsw.model.board.TargetCells;
 import it.polimi.ingsw.model.workers.WorkerID;
 import it.polimi.ingsw.utils.networking.ClientHandleable;
 
@@ -11,9 +10,10 @@ import java.util.List;
 public class ServerAskMoveMessage implements ServerMessage, ClientHandleable {
     public final boolean isSkippable;
     public final List<WorkerID> allowedWorkers;
-    public final HashMap<WorkerID, TargetCells> workerBlockBuildableCells;
+    public final HashMap<WorkerID, ReducedTargetCells> workerBlockBuildableCells;
 
-    public ServerAskMoveMessage(boolean isSkippable, List<WorkerID> allowedWorkers, HashMap<WorkerID, TargetCells> workerBlockBuildableCells) {
+    public ServerAskMoveMessage(boolean isSkippable, List<WorkerID> allowedWorkers,
+                                HashMap<WorkerID, ReducedTargetCells> workerBlockBuildableCells) {
         this.isSkippable = isSkippable;
         this.allowedWorkers = allowedWorkers;
         this.workerBlockBuildableCells = workerBlockBuildableCells;
