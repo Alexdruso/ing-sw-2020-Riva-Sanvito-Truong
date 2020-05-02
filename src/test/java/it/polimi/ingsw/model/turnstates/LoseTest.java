1fd89db08a281ef4b28be4ccd13aa3dd4e4b3947
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-05-02 14:31:14 +0200

    test: fix states test

--- src/test/java/it/polimi/ingsw/model/turnstates/LoseTest.java
+++ src/test/java/it/polimi/ingsw/model/turnstates/LoseTest.java
@@ -1,18 +1,21 @@
 package it.polimi.ingsw.model.turnstates;
 
+import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.Turn;
 import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class LoseTest {
     Turn mockTurn = mock(Turn.class);
+    Game mockGame = mock(Game.class);
     Lose loseTest = new Lose();
 
     @Test
     void setup() {
+        when(mockTurn.getGame()).thenReturn(mockGame);
         loseTest.setup(this.mockTurn);
         verify(this.mockTurn).setNextState(TurnState.LOSE.getTurnState());
+        verify(this.mockGame).triggerLosingTurn();
     }
 }
--- src/test/java/it/polimi/ingsw/model/turnstates/WinTest.java
+++ src/test/java/it/polimi/ingsw/model/turnstates/WinTest.java
@@ -1,18 +1,21 @@
 package it.polimi.ingsw.model.turnstates;
 
+import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.Turn;
 import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class WinTest {
     Turn mockTurn = mock(Turn.class);
+    Game mockGame = mock(Game.class);
     Win winTest = new Win();
 
     @Test
     void setup() {
+        when(mockTurn.getGame()).thenReturn(mockGame);
         this.winTest.setup(this.mockTurn);
         verify(this.mockTurn).setNextState(TurnState.WIN.getTurnState());
+        verify(this.mockGame).triggerWinningTurn();
     }
 }
