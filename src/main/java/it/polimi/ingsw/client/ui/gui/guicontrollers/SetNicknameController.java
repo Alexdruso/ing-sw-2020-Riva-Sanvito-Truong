35735ce762f50136c9c258e268b89c8e2bb01803
Kien Tuong Truong <kientuong@gmail.com>
2020-05-09 16:51:46 +0200

    fix: add nickname max length check to avoid rendering issues

--- src/main/java/it/polimi/ingsw/client/ui/gui/guicontrollers/SetNicknameController.java
+++ src/main/java/it/polimi/ingsw/client/ui/gui/guicontrollers/SetNicknameController.java
@@ -30,10 +30,16 @@ public class SetNicknameController extends AbstractController{
 
     @FXML
     public void handleNicknameButton(ActionEvent event){
-        if(nicknameField.getLength() != 0){
-            ((SetNicknameGUIClientState)state).setNickname(nicknameField.getText());
+        if(nicknameField.getLength() != 0) {
+            if(nicknameField.getLength() >= 30){
+                //Just to not mess up the rendering of the nickname
+                errorLabel.setText(I18n.string(I18nKey.ERROR_INVALID_NICKNAME_LENGTH));
+                errorLabel.setOpacity(1);
+            } else {
+                ((SetNicknameGUIClientState) state).setNickname(nicknameField.getText());
+            }
         } else {
-            errorLabel.setText(I18n.string(I18nKey.ERROR_INVALID_NICKNAME));
+            errorLabel.setText(I18n.string(I18nKey.ERROR_INVALID_NICKNAME_GENERIC));
             errorLabel.setOpacity(1);
         }
     }
