63d03eb2dadfb3695954bb13e3ec3cc3d6ae9f87
Kien Tuong Truong <kientuong@gmail.com>
2020-05-10 19:23:21 +0200

    chore: add fixme as reminder

--- src/main/java/it/polimi/ingsw/client/ui/gui/guicontrollers/JoinLobbyController.java
+++ src/main/java/it/polimi/ingsw/client/ui/gui/guicontrollers/JoinLobbyController.java
@@ -40,6 +40,8 @@ public class JoinLobbyController extends AbstractController {
         welcomeLabel.setText(String.format(I18n.string(I18nKey.WELCOME_TO_THE_SERVER_S), client.getNickname()));
         final int[] imgCount = {0};
         animationTimer = new Timer();
+        //FIXME: I believe that this timer keeps the Client from shutting down when closing the window
+        //Maybe submit all Timer Tasks to a global instance of Timer so it can stop all of them?
         animationTimer.schedule(new TimerTask() {
             @Override
             public void run() {
