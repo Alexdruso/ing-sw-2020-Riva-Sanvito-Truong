41c4f1beb521ebaf28a6041094b393479a783807
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-05-13 22:47:00 +0200

    feat: complete Triton

--- src/main/java/it/polimi/ingsw/model/gods/Triton.java
+++ src/main/java/it/polimi/ingsw/model/gods/Triton.java
@@ -1,9 +1,57 @@
 package it.polimi.ingsw.model.gods;
 
+import it.polimi.ingsw.model.Turn;
+import it.polimi.ingsw.model.actions.MoveAction;
+import it.polimi.ingsw.model.board.Board;
+import it.polimi.ingsw.model.board.Cell;
+import it.polimi.ingsw.model.turnevents.TurnEvents;
+import it.polimi.ingsw.model.turnstates.TurnState;
+
+import java.util.List;
+
 /**
  * The god card Triton.
  */
 public class Triton extends AbstractGod {
+    /**
+     * The TurnEvents for the owner of the Triton god card.
+     */
+    private static final TurnEvents ownerTurnEvents = new TurnEvents() {
+        @Override
+        protected void onBeforeMovement(Turn turn) {
+            List<MoveAction> moveActions = turn.getMoves();
+            MoveAction lastMove = moveActions.get(moveActions.size() - 1);
+            Cell targetCell = lastMove.getTargetCell();
+            Board board = turn.getGame().getBoard();
+            //if last move was on border, then we can skip this move
+            if (
+                    targetCell.getX() == 0
+                            || targetCell.getX() == board.getDimension() - 1
+                            || targetCell.getY() == 0
+                            || targetCell.getY() == board.getDimension() - 1
+            ) {
+                turn.setSkippable(true);
+            }
+        }
+
+        @Override
+        protected void onAfterMovement(Turn turn) {
+            List<MoveAction> moveActions = turn.getMoves();
+            MoveAction lastMove = moveActions.get(moveActions.size() - 1);
+            Cell targetCell = lastMove.getTargetCell();
+            Board board = turn.getGame().getBoard();
+            //if last move was on border, then we can move again
+            if (
+                    targetCell.getX() == 0
+                            || targetCell.getX() == board.getDimension() - 1
+                            || targetCell.getY() == 0
+                            || targetCell.getY() == board.getDimension() - 1
+            ) {
+                turn.setNextState(TurnState.MOVE.getTurnState());
+            }
+        }
+    };
+
     /**
      * Gets the god's name.
      *
@@ -12,5 +60,15 @@ public class Triton extends AbstractGod {
     @Override
     public String getName() {
         return "Triton";
+    }
+
+    /**
+     * Gets the TurnEvents for the player owning Triton.
+     *
+     * @return the TurnEvents for the player owning Triton
+     */
+    @Override
+    public TurnEvents getOwnerTurnEvents() {
+        return ownerTurnEvents;
     }
 }
