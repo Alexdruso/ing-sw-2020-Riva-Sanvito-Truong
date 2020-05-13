d2b3f4aa557ff12dc6aaa59df8948450c32f15e8
Alessandro <alessandro1.sanvito@mail.polimi.it>
2020-05-13 23:02:39 +0200

    feat: add Zeus class and code

--- src/main/java/it/polimi/ingsw/model/gods/Zeus.java
+++ src/main/java/it/polimi/ingsw/model/gods/Zeus.java
@@ -1,9 +1,28 @@
 package it.polimi.ingsw.model.gods;
 
+import it.polimi.ingsw.model.Turn;
+import it.polimi.ingsw.model.turnevents.TurnEvents;
+
+import java.util.Arrays;
+
 /**
  * The god card Zeus.
  */
 public class Zeus extends AbstractGod {
+    /**
+     * The TurnEvents for the owner of the Triton god card.
+     */
+    private static final TurnEvents ownerTurnEvents = new TurnEvents() {
+        @Override
+        protected void onBeforeBuild(Turn turn) {
+            Arrays.stream(turn.getPlayer().getWorkers())
+                    .forEach(
+                            worker -> turn.getWorkerBlockBuildableCells(worker)
+                                    .setPosition(worker.getCell(), true)
+                    );
+        }
+    };
+
     /**
      * Gets the god's name.
      *
@@ -12,5 +31,15 @@ public class Zeus extends AbstractGod {
     @Override
     public String getName() {
         return "Zeus";
+    }
+
+    /**
+     * Gets the TurnEvents for the player owning Zeus.
+     *
+     * @return the TurnEvents for the player owning Zeus
+     */
+    @Override
+    public TurnEvents getOwnerTurnEvents() {
+        return ownerTurnEvents;
     }
 }
