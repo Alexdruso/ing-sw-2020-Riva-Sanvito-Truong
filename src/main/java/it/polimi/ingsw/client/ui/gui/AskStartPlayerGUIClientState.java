fc86419413b9c04f56e66d06219c8ba202c1b5be
Kien Tuong Truong <kientuong@gmail.com>
2020-05-18 18:24:16 +0200

    feat: add ask start player scene

--- src/main/java/it/polimi/ingsw/client/ui/gui/AskStartPlayerGUIClientState.java
+++ src/main/java/it/polimi/ingsw/client/ui/gui/AskStartPlayerGUIClientState.java
@@ -2,6 +2,9 @@ package it.polimi.ingsw.client.ui.gui;
 
 import it.polimi.ingsw.client.Client;
 import it.polimi.ingsw.client.clientstates.AbstractAskStartPlayerClientState;
+import it.polimi.ingsw.client.clientstates.ClientState;
+import it.polimi.ingsw.client.ui.gui.utils.SceneLoaderFactory;
+import it.polimi.ingsw.utils.messages.ReducedUser;
 
 public class AskStartPlayerGUIClientState extends AbstractAskStartPlayerClientState implements GUIClientState{
     /**
@@ -13,6 +16,16 @@ public class AskStartPlayerGUIClientState extends AbstractAskStartPlayerClientSt
         super(client);
     }
 
+    public void returnToMenu(){
+        client.moveToState(ClientState.WELCOME_SCREEN);
+        client.closeConnection();
+    }
+
+    public void selectUser(ReducedUser user){
+        chosenUser = user;
+        notifyUiInteraction();
+    }
+
     /**
      * Function called by the main thread that renders the current state to the UI.
      * This function is the only one of this class allowed to be synchronous with the user input.
@@ -22,6 +35,7 @@ public class AskStartPlayerGUIClientState extends AbstractAskStartPlayerClientSt
      */
     @Override
     public void render() {
-
+        SceneLoaderFactory sceneLoaderFactory = new SceneLoaderFactory("/fxml/AskStartPlayer.fxml", client);
+        sceneLoaderFactory.setState(ClientState.ASK_START_PLAYER, this).build().executeSceneChange();
     }
 }
---
+++ src/main/java/it/polimi/ingsw/client/ui/gui/guicontrollers/AskStartPlayerController.java
@@ -0,0 +1,55 @@
+package it.polimi.ingsw.client.ui.gui.guicontrollers;
+
+import it.polimi.ingsw.client.reducedmodel.ReducedPlayer;
+import it.polimi.ingsw.client.ui.gui.AskStartPlayerGUIClientState;
+import it.polimi.ingsw.utils.messages.ReducedUser;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AskStartPlayerController extends AbstractController{
+
+    @FXML
+    VBox playerButtonsPane;
+
+    @FXML
+    Label errorLabel;
+
+    private List<ReducedUser> users;
+
+    @Override
+    public void setupController(){
+        errorLabel.setOpacity(0);
+        users = client.getGame()
+                .getPlayersList()
+                .stream()
+                .map(ReducedPlayer::getUser)
+                .collect(Collectors.toList());
+
+        for(ReducedUser user: users){
+            Button userButton = new Button();
+            userButton.setText(user.nickname);
+            userButton.getStyleClass().add("bigbutton");
+            userButton.setMaxWidth(400);
+            userButton.setPrefWidth(400);
+            userButton.setPrefHeight(60);
+            userButton.setOnAction(e -> ((AskStartPlayerGUIClientState)state).selectUser(user));
+            playerButtonsPane.getChildren().add(userButton);
+        }
+    }
+
+    @FXML
+    public void handleMenuButton(ActionEvent event){
+        ((AskStartPlayerGUIClientState)state).returnToMenu();
+    }
+
+    @Override
+    public void handleError(String message) {
+
+    }
+}
