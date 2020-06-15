ae695a1182d90fae1d44a999fe92d13c58fde946
Kien Tuong Truong <kientuong@gmail.com>
2020-06-15 14:56:32 +0200

    feat: add disconnect screen

--- src/main/java/it/polimi/ingsw/client/ui/gui/DisconnectGUIClientState.java
+++ src/main/java/it/polimi/ingsw/client/ui/gui/DisconnectGUIClientState.java
@@ -2,6 +2,9 @@ package it.polimi.ingsw.client.ui.gui;
 
 import it.polimi.ingsw.client.Client;
 import it.polimi.ingsw.client.clientstates.AbstractDisconnectClientState;
+import it.polimi.ingsw.client.clientstates.ClientState;
+import it.polimi.ingsw.client.ui.gui.utils.SceneLoader;
+import it.polimi.ingsw.client.ui.gui.utils.SceneLoaderFactory;
 
 public class DisconnectGUIClientState extends AbstractDisconnectClientState implements GUIClientState{
     /**
@@ -13,6 +16,17 @@ public class DisconnectGUIClientState extends AbstractDisconnectClientState impl
         super(client);
     }
 
+    public void reconnect(){
+        SceneLoader.applyBlurOut(500);
+        notifyUiInteraction();
+    }
+
+    public void returnToMenu(){
+        SceneLoader.applyBlurOut(2000);
+        client.closeConnection();
+        client.moveToState(ClientState.WELCOME_SCREEN);
+    }
+
     /**
      * Function called by the main thread that renders the current state to the UI.
      * This function is the only one of this class allowed to be synchronous with the user input.
@@ -22,14 +36,12 @@ public class DisconnectGUIClientState extends AbstractDisconnectClientState impl
      */
     @Override
     public void render() {
-
-    }
-
-    /**
-     * Function called by the UI after the user has provided his/her inputs.
-     */
-    @Override
-    public void notifyUiInteraction() {
-
+        SceneLoaderFactory sceneLoaderFactory = new SceneLoaderFactory("/fxml/Disconnect.fxml", client);
+        sceneLoaderFactory.setState(ClientState.DISCONNECT, this)
+                .setFadeIn(false)
+                .setFadeOut(false)
+                .setReplaceOldScene(false)
+                .build()
+                .executeSceneChange();
     }
 }
---
+++ src/main/java/it/polimi/ingsw/client/ui/gui/guicontrollers/DisconnectController.java
@@ -0,0 +1,29 @@
+package it.polimi.ingsw.client.ui.gui.guicontrollers;
+
+import it.polimi.ingsw.client.ui.gui.DisconnectGUIClientState;
+import it.polimi.ingsw.client.ui.gui.LoseGameGUIClientState;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DisconnectController extends AbstractController {
+    private static final Logger LOGGER = Logger.getLogger(DisconnectController.class.getName());
+
+    @FXML
+    private void handleReconnect(ActionEvent event){
+        ((DisconnectGUIClientState)state).reconnect();
+    }
+
+    @FXML
+    private void handleMenu(ActionEvent event){
+            ((DisconnectGUIClientState)state).returnToMenu();
+    }
+
+    @Override
+    public void handleError(String message) {
+        LOGGER.log(Level.SEVERE, message);
+
+    }
+}
