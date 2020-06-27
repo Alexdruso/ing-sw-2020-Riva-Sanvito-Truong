981659cdc275ff6c9c0c1f0654639c53c0fda34f
Kien Tuong Truong <kientuong@gmail.com>
2020-06-27 15:46:30 +0200

    refactor: change language buttons display

--- src/main/java/it/polimi/ingsw/client/ui/gui/guicontrollers/SettingsController.java
+++ src/main/java/it/polimi/ingsw/client/ui/gui/guicontrollers/SettingsController.java
@@ -4,9 +4,13 @@ import it.polimi.ingsw.client.ui.gui.GUI;
 import it.polimi.ingsw.client.ui.gui.utils.SceneLoaderFactory;
 import it.polimi.ingsw.utils.i18n.AvailableLocale;
 import it.polimi.ingsw.utils.i18n.I18n;
+import it.polimi.ingsw.utils.structures.BidirectionalHashMap;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -14,8 +18,11 @@ import java.util.logging.Logger;
 public class SettingsController extends AbstractController {
     private static final Logger LOGGER = Logger.getLogger(SettingsController.class.getName());
 
+    private final BidirectionalHashMap<AvailableLocale, ToggleButton> buttonMap = new BidirectionalHashMap<>();
+    private ToggleGroup toggleGroup;
+
     @FXML
-    ChoiceBox<AvailableLocale> languageChoiceBox;
+    HBox languageButtons;
 
     @FXML
     public void handleMenuButton(ActionEvent event){
@@ -29,17 +36,26 @@ public class SettingsController extends AbstractController {
 
     @Override
     public void onSceneShow(){
-        languageChoiceBox.setValue(AvailableLocale.fromLocale(I18n.getLocale()));
+        buttonMap.getValueFromKey(AvailableLocale.fromLocale(I18n.getLocale())).setSelected(true);
     }
 
     @FXML
     public void initialize(){
-        languageChoiceBox.getItems().addAll(AvailableLocale.values());
+        toggleGroup = new ToggleGroup();
+        for(AvailableLocale locale: AvailableLocale.values()){
+            ToggleButton button = new ToggleButton(locale.toString());
+            button.setToggleGroup(toggleGroup);
+            button.setGraphic(new ImageView(locale.icon));
+            button.setPrefHeight(70);
+            button.setPrefWidth(200);
+            buttonMap.put(locale, button);
+            languageButtons.getChildren().add(button);
+        }
     }
 
     @FXML
     public void saveSettings(ActionEvent e){
-        I18n.setLocale(languageChoiceBox.getValue().locale);
+        I18n.setLocale(buttonMap.getKeyFromValue((ToggleButton)toggleGroup.getSelectedToggle()).locale);
         ((GUI)client.getUI()).clearSceneMap();
         switchToMenu();
     }
